625f63057ee5c5b81461d48d3abd0a0b77015170
giosue <giosue_liuzzo@hotmail.be>
2020-04-12 08:04:50 +0200

    Correction des vue Detail

--- src/app/core/models/Film.detail.model.ts
+++ src/app/core/models/Film.detail.model.ts
@@ -1,18 +1,35 @@
-export interface Personnes {
+export interface Realisateurs {
+  id: number;
   nom: string;
   prenom: string;
+  ad_rue: string;
+  ad_code_postal: number;
+  ad_ville: string;
+  tel?: any;
 }
 
-export interface Realisateurs {
+export interface _films {
+  id: number;
+  titre: string;
+  description: string;
+  date_sortie: string;
+  bande_annoce: string;
+  image: string;
+  realisateurs: Realisateurs;
+}
+
+export interface _acteurs {
+  id: number;
   nom: string;
   prenom: string;
+  ad_rue: string;
+  ad_code_postal: number;
+  ad_ville: string;
+  tel?: any;
 }
 
 export interface FilmDetailModel {
   id: number;
-  titre: string;
-  date_sortie: string;
-  bande_annoce: string;
-  personnes: Personnes[];
-  realisateurs: Realisateurs;
+  _films: _films;
+  _acteurs: _acteurs;
 }
